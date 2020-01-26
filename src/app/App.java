9600f83004eb8af8407ab880bd5e423ff65dc768
Odohi David <odohi.david@gmail.com>
2020-01-26 13:53:36 +0100

    read filename from command line argument

--- src/app/App.java
+++ src/app/App.java
@@ -2,7 +2,13 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ConfigParser configParser = new ConfigParser("config.txt");
+        String fileName = null;
+        try {
+            fileName = args[0];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println(e.getClass().getSimpleName() + " : " + e.getMessage());
+        }
+        ConfigParser configParser = new ConfigParser(fileName);
         System.out.println(configParser.get("application.name"));
     }
 }
